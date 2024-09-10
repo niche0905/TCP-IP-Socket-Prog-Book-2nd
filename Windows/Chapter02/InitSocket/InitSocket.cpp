46dbfa843241cf0a3bc6baa0e4498c5e0291e236
niche0905 <yudle537@gmail.com>
2024-09-10 14:58:05 +0900

    UDP로 소켓 생성

--- Windows/Chapter02/InitSocket/InitSocket.cpp
+++ Windows/Chapter02/InitSocket/InitSocket.cpp
@@ -1,15 +1,21 @@
 #include "..\..\Common.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
 	// 윈속 초기화
 	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	if (WSAStartup(MAKEWORD(1, 1), &wsa) != 0)
 		return 1;
 	printf("[알림] 윈속 초기화 성공\n");
 
+	std::cout << wsa.wVersion << std::endl;
+	std::cout << wsa.wHighVersion << std::endl;
+	std::cout << wsa.szDescription << std::endl;
+	std::cout << wsa.szSystemStatus << std::endl;
+
 	// 소켓 생성
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 	printf("[알림] 소켓 생성 성공\n");
 
